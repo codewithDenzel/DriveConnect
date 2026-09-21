8bbc1dc6cc596ab6d0a5d01532efdb9dc6a9115a
Denzel Roi Sinio <255033429+codewithDenzel@users.noreply.github.com>
2026-09-21 22:33:35 +0800

    Restore working master CRM data structure

--- DriveConnect.domain/Entities/InteractionLog.cs
+++ DriveConnect.domain/Entities/InteractionLog.cs
@@ -9,9 +9,7 @@ namespace DriveConnect.domain.Entities
     {
         [Key]
         public int InteractionId { get; set; }
-        public string? FirstName { get; set; }
-        public string? MiddleName { get; set; }
-        public string? LastName { get; set; }
+        public string? CustomerName { get; set; }
         public string? PhoneNumber { get; set; }
         public string? InteractionType { get; set; }
         public string? Subject { get; set; }
