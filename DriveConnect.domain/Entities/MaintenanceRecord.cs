27cd46e9a9a913070b0e41707be9a4cd6e32d3d4
Denzel Roi Sinio <255033429+codewithDenzel@users.noreply.github.com>
2026-09-21 22:33:46 +0800

    Restore working master CRM data structure

--- DriveConnect.domain/Entities/MaintenanceRecord.cs
+++ DriveConnect.domain/Entities/MaintenanceRecord.cs
@@ -9,9 +9,7 @@ namespace DriveConnect.domain.Entities
     {
         [Key]
         public int MaintenanceId { get; set; }
-        public string? FirstName { get; set; }
-        public string? MiddleName { get; set; }
-        public string? LastName { get; set; }
+        public string? CustomerName { get; set; }
         public string? PhoneNumber { get; set; }
         public string? VehicleModel { get; set; }
         public DateTime ServiceDate { get; set; }
